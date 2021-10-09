be3196589d95e7cd61328ebf31f76dd060400142
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-10-09 11:55:51 +0300

    add authReducer.tsx

---
+++ Redux/authReducer.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+
+let initialState: InitialStateType = {
+    data:
+        {
+    id: 19583,
+    email: 'pasha17061987@gmail.com',
+    login: 'Pavel1787'
+    },
+
+}
+
+type DataType = {
+        id: number | null
+        email: string | null
+        login: string | null
+
+}
+
+export type InitialStateType = {
+    data: DataType
+
+
+}
+
+export const authReducer = (state = initialState, action: UsersActionsType): InitialStateType => {
+
+    switch (action.type) {
+        case "SET-USER-DATA":{
+            return {...state,
+            data: action.data}
+        }
+
+        default:
+            return state
+    }
+}
+
+
+export const setUserDataAC = (id:number, email:string, login:string) => {
+    return {
+        type: 'SET-USER-DATA',
+        data: {
+            id:id,
+            email:email,
+            login:login,
+        }
+    } as const
+}
+
+type setUserDataACType = ReturnType<typeof setUserDataAC>
+
+export type UsersActionsType =  setUserDataACType
+

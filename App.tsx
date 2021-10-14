2ce2606d394fd3987d97699bc3e3a67eedb8e2c9
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-10-14 10:18:40 +0300

    add redirect APP.tsx

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import {store} from "./Redux/State";
 import {UsersContainer} from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Face/ProfileContainer";
 import HeaderContainer from './components/Header/HeaderContainer';
+import {Login} from "./components/LoginComponent/Login";
 
 
 
@@ -28,10 +29,12 @@ const App: React.FC = () => {
                     <Route path='/conversation' render={()=><Conversation  />}/>
                     <Route path='/profile/:userId?' render={()=><ProfileContainer />}/>
                     <Route path='/users' render={()=><UsersContainer />}/>
+                    <Route path='/login' render={()=><Login />}/>
 
                     <Route path='/news' />
                     <Route path='/music' />
-                    <Route path='/settings' /><Route path='/friends' render={()=> <Friends dialogs={store._state.dialogsPage.dialogs} />}/>
+                    <Route path='/settings' />
+                    <Route path='/friends' render={()=> <Friends dialogs={store._state.dialogsPage.dialogs} />}/>
                 </div>
             </div>
 

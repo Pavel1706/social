34abdc0a1d03005308ac9a96fa705b5cad9c5af1
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-10-18 01:21:03 +0300

    upgrade reduxStore.tsx

--- Redux/reduxStore.tsx
+++ Redux/reduxStore.tsx
@@ -1,9 +1,11 @@
-import {combineReducers, createStore} from "redux";
-import {profileReducer} from "./profileReducer";
-import {messageReducer} from "./messageReducer";
-import {dialogsReducer} from "./dialogsReducer";
-import {usersReducer} from "./usersReducer";
-import {authReducer} from "./authReducer";
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import {ProfileActionsType, profileReducer} from "./profileReducer";
+import {messageReducer, NewMessageActionType} from "./messageReducer";
+import {dialogsReducer, DialogType} from "./dialogsReducer";
+import {UsersActionsType, usersReducer} from "./usersReducer";
+import {authReducer, LoginActionsType} from "./authReducer";
+import thunkMiddleware, { ThunkAction } from 'redux-thunk'
+import { reducer as formReducer } from 'redux-form'
 
 export let rootReducer = combineReducers({
     profilePage: profileReducer,
@@ -11,10 +13,17 @@ export let rootReducer = combineReducers({
     dialogsPage: dialogsReducer,
     users: usersReducer,
     auth: authReducer,
+    form: formReducer
 })
 
-export let store = createStore(rootReducer)
+export let store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 
 export type AppStateType = ReturnType<typeof rootReducer>
+export type AppActionsType = UsersActionsType | LoginActionsType
+    | ProfileActionsType | DialogType | NewMessageActionType
+
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType,AppStateType,unknown,AppActionsType>
+//@ts-ignore
+window.store = store
 // export type StoreType = typeof store;
 // export type StateType = ReturnType<typeof store.getState>;

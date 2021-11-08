de59c0533bceaa764602217e5591d9d7f888f063
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-11-08 14:52:24 +0300

    change Friends.tsx

--- components/Friends/Friends.tsx
+++ components/Friends/Friends.tsx
@@ -10,7 +10,6 @@ type ConversationArrayType = {
 export const Friends =(props:ConversationArrayType)=>{
 
     return(
-
         <span className={style.avatar} >
 
             {props.dialogs.map(t=>  <div key={t.id}> <img alt={'foto'} className={style.avatar} src={t.foto} />{t.name}</div> )}
